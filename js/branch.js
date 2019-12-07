8b439e0f794004272387ff585c87a3ae9f596ddd
Dusan Rasic <dusan@Dusans-MacBook-Pro.local>
2019-12-07 15:39:32 +0100

    adding animation to tree

--- js/branch.js
+++ js/branch.js
@@ -2,34 +2,27 @@ function Branch(begin, end) {
 	this.begin = begin;
 	this.end = end;
 	this.finished = false;
-	this.angle = 0;
-	angleMode(DEGREES)
+	angleMode(DEGREES);
 
 	this.show = () => {
 		stroke(255);
 		line(this.begin.x, this.begin.y, this.end.x, this.end.y);
 	}
 
-	this.update = (a) => {
-		this.angle = a;
-		// console.log(this.angle);
-	}
-
-	this.branchA = () => {
-		var dir = p5.Vector.sub(this.end, this.begin);
-		dir.rotate(30 + this.angle);
+	this.branchA = (alpha) => {
+		let dir = p5.Vector.sub(this.end, this.begin);
+		dir.rotate(30 + alpha);
 		dir.mult(0.7);
-		var newEnd = p5.Vector.add(this.end, dir);
-		var b = new Branch(this.end, newEnd);
+		let newEnd = p5.Vector.add(this.end, dir);
+		let b = new Branch(this.end, newEnd);
 		return b;
 	}
-	this.branchB= () => {
-		var dir = p5.Vector.sub(this.end, this.begin);
-		dir.rotate(-30 + this.angle);
+	this.branchB = () => {
+		let dir = p5.Vector.sub(this.end, this.begin);
+		dir.rotate(-30 + alpha);
 		dir.mult(0.7);
-		var newEnd = p5.Vector.add(this.end, dir);
-		var b = new Branch(this.end, newEnd);
-
+		let newEnd = p5.Vector.add(this.end, dir);
+		let b = new Branch(this.end, newEnd);
 		return b;
 	}
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,70 +1,68 @@
-var tree = [];
-var leaves = [];
-var count = 0;
-var _b = null;
+let tree = [];
+let leaves = [];
+let count = 0;
+let alpha = 0;
 
-function setup() {
+setup = () => {
 	createCanvas(windowWidth, windowHeight);
 	frameRate(120);
-	var a = createVector(width/2, height);
-	var b = createVector(width/2, height-100);
 
-	var root = new Branch(a, b);
-
-	tree[0] = root;
-	for(var i=0; i < 6; i++) {
-		addBranches();
-		count++;
-	}
+	setInterval(() => {
+		this.moveTree();
+	}, 500);
 }
 
-function addBranches() {
-	for(var i = tree.length - 1; i >= 0; i--) {
+addBranches = () => {
+	for(let i = tree.length - 1; i >= 0; i--) {
 		if(!tree[i].finished) {
-			tree.push(tree[i].branchA());
-			tree.push(tree[i].branchB());
+			tree.push(tree[i].branchA(alpha));
+			tree.push(tree[i].branchB(alpha));
 		}
 		tree[i].finished = true;
 	}
 
 	if(count === 5) {
-		for(var i=0; i < tree.length; i++) {
-			var leaf = tree[i].end.copy();
+		for(let i=0; i < tree.length; i++) {
+			let leaf = tree[i].end.copy();
 			leaves.push(leaf);
 		}
 	}
 }
 
-function normalize (val, max, min) {
-	return (val-min)/(max-min);
+moveTree = () => {
+	alpha = Math.floor(Math.random() * 2) + 1;
+	alpha *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+	this.setupTree();
 }
 
-function mouseMoved() {
-	if(mouseX > width/2) {
-		for(var i=10; i < tree.length; i++) {
-			a = normalize(mouseX-width/2, width/2, 0);
-			_b = tree[i];
-			_b.update(a*30);
-			redraw();
-		}
-	}
-	else if(mouseX < width/2){
-		for(var i=10; i < tree.length; i++) {
-			a = -normalize(width/2-mouseX, width/2, 0);
-			_b = tree[i];
-			_b.update(a*30);
-			redraw();
-		}
+setupTree = () => {
+	this.resetValues();
+	let a = createVector(width/2, height);
+	let b = createVector(width/2, height-100);
+
+	let root = new Branch(a, b);
+
+	tree[0] = root;
+	for(let i=0; i < 6; i++) {
+		addBranches();
+		count++;
 	}
 }
 
-function draw() {
+resetValues = () => {
+	tree = [];
+	leaves = [];
+	count = 0;
+}
+
+draw = () => {
+	this.setupTree();
 	background(51);
 
-	for(var i=0; i<tree.length; i++) {
+	for(let i=0; i<tree.length; i++) {
 		tree[i].show();
 	}
-	for(var i=0; i < leaves.length; i++) {
+	for(let i=0; i < leaves.length; i++) {
 		fill(77, 168, 59, 150);
 		noStroke();
 		ellipse(leaves[i].x, leaves[i].y, 8, 8);
